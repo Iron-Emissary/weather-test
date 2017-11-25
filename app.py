3cab10948ae8874a1beb8f68046902a3267c8c2a
Iron-Emissary <33970903+Iron-Emissary@users.noreply.github.com>
2017-11-25 17:22:09 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,4 @@
-# -*- coding:utf8 -*-
-# !/usr/bin/env python
-# Copyright 2017 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/usr/bin/env python
 
 from __future__ import print_function
 from future.standard_library import install_aliases
@@ -88,7 +74,7 @@ def makeWebhookResult(data):
 
     item = channel.get('item')
     location = channel.get('location')
-    units = channel.get('units')
+    #units = channel.get('units')
     if (location is None) or (item is None) or (units is None):
         return {}
 
@@ -98,8 +84,9 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
-             ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') 
+        #+ " " + units.get('temperature')
 
     print("Response:")
     print(speech)
